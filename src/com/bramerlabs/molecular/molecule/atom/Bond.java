7bc7dd18d4ebc8cb8e008360d65d68aa2697846f
Bo Bramer <bramer.bo@gmail.com>
2020-11-22 17:56:40 -0800

    Added Bond class, began adding variables

---
+++ src/com/bramerlabs/molecular/molecule/atom/Bond.java
@@ -0,0 +1,14 @@
+package com.bramerlabs.molecular.molecule.atom;
+
+public class Bond {
+
+    private Atom a1, a2;
+
+    private float length;
+    private int num; // single 1, double 2, triple 3, etc.
+
+    public Bond() {
+
+    }
+
+}
