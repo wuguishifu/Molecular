19d571c855ecb9482819c14a058629426d49cac8
Bo Bramer <bramer.bo@gmail.com>
2021-01-23 17:29:05 -0800

    Began work on substituents.

--- src/com/bramerlabs/molecular/molecule/functional_groups/CarboxylicAcid.java
+++ src/com/bramerlabs/molecular/molecule/functional_groups/CarboxylicAcid.java
@@ -1,4 +1,13 @@
 package com.bramerlabs.molecular.molecule.functional_groups;
 
+import com.bramerlabs.engine.math.Vector3f;
+
 public class CarboxylicAcid {
+
+    private Vector3f position;
+
+    public CarboxylicAcid(Vector3f position) {
+        this.position = position;
+    }
+
 }
